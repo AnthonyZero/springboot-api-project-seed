fd9f6464356935156ce8f116f47296a361c62386
736252868@qq.com <736252868@qq.com>
2018-07-12 11:33:32 +0800

    :blossom:MBG生成mapper等代码

--- src/test/java/com/anthonyzero/seed/ApplicationTest.java
+++ src/test/java/com/anthonyzero/seed/ApplicationTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alibaba.fastjson.JSON;
-import com.anthonyzero.seed.modules.sys.domain.SmUser;
-import com.anthonyzero.seed.modules.sys.mapper.SmUserMapper;
+import com.anthonyzero.seed.modules.user.domain.SmUser;
+import com.anthonyzero.seed.modules.user.mapper.SmUserMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
